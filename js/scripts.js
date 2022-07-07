3162c7401f83168cfa50d9d95b5f833d184546e5
shayalieb <105383935+shayalieb@users.noreply.github.com>
2022-07-07 12:49:33 -0400

    All the practive changes

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//propor form from top to button for JS to proporly rendor
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+//Calculations
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+//Add in sizing 
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
